85df2d6f2962ac2ba8bea975d82a8b7f4331e711
aib <aibok42@gmail.com>
2017-05-31 03:02:47 +0300

    Run and connect the Telegram and IRC runners

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ logging.basicConfig(level=1)
 _logger = logging.getLogger(__name__)
 
 import configparser
+import threading
+import time
 
 import ircrunner
 import tgrunner
@@ -19,9 +21,22 @@ class MyConfigParser(configparser.ConfigParser):
 	def save(self):
 		_logger.debug("Saving configuration")
 
-conf = MyConfigParser('config.cfg')
+def main():
+	conf = MyConfigParser('config.cfg')
+	tgr = tgrunner.TGRunner(conf)
+	ircr = ircrunner.IRCRunner(conf)
 
-#tgr = tgrunner.TGRunner(conf)
-#tgr.run()
-#ircr = ircrunner.IRCRunner(conf)
-#ircr.run()
+	tgr.set_irc_messager(ircr.forward_message)
+	ircr.set_telegram_messager(tgr.forward_message)
+
+	_logger.debug("Starting TGRunner thread")
+	threading.Thread(target=tgr.run).start()
+
+	_logger.debug("Starting IRCRunner thread")
+	threading.Thread(target=ircr.run).start()
+
+	while True:
+		time.sleep(1)
+
+if __name__ == '__main__':
+	main()
